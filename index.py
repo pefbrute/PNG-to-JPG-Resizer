7f81b7074a3087f2fc16eb9add0f7326c15b3745
Fedor Pasynkov <76725612+pefbrute@users.noreply.github.com>
2023-12-08 08:34:40 +0530

    Update index.py

--- index.py
+++ index.py
@@ -5,9 +5,6 @@ import os
 import subprocess
 import argparse
 
-DEFAULT_WIDTH = 6000
-DEFAULT_HEIGHT = 6000
-
 def read_image(image_path):
     """Reads an image from the specified path."""
     image = cv2.imread(image_path)
@@ -15,8 +12,10 @@ def read_image(image_path):
         raise IOError(f"Could not open or find the image {image_path}.")
     return image
 
-def resize_image(image, new_width, new_height):
-    """Resizes the given image to the specified width and height."""
+def resize_image(image, scale_factor):
+    """Resizes the given image by a scale factor."""
+    new_width = int(image.shape[1] * scale_factor)
+    new_height = int(image.shape[0] * scale_factor)
     return cv2.resize(image, (new_width, new_height), interpolation=cv2.INTER_AREA)
 
 def save_image(image, image_path, format=".png"):
@@ -33,11 +32,11 @@ def convert_to_jpg(image_path):
     subprocess.run(["convert", image_path, output_file], check=True)
     return output_file
 
-def process_image(file_path, width, height):
+def process_image(file_path, scale_factor=5):
     """Process the image: resize and convert to JPG."""
     try:
         image = read_image(file_path)
-        resized_image = resize_image(image, width, height)
+        resized_image = resize_image(image, scale_factor)
         resized_image_path = save_image(resized_image, file_path)
         converted_image_path = convert_to_jpg(resized_image_path)
         print(f"Processed {file_path}: Resized to {resized_image_path}, Converted to {converted_image_path}")
@@ -47,12 +46,10 @@ def process_image(file_path, width, height):
 def main():
     parser = argparse.ArgumentParser(description="Resize and convert images.")
     parser.add_argument('images', nargs='+', help='Image files to process')
-    parser.add_argument('--width', type=int, default=DEFAULT_WIDTH, help='New width of images')
-    parser.add_argument('--height', type=int, default=DEFAULT_HEIGHT, help='New height of images')
     args = parser.parse_args()
 
     for file_path in args.images:
-        process_image(file_path, args.width, args.height)
+        process_image(file_path)
 
 if __name__ == "__main__":
     main()
